78b9d6c3bbe2217ed4f7f0604edee1c2c6c187b1
Chester <chester@easybotics.com>
2025-07-16 23:11:01 -1000

    Update .gitignore to exclude Cursor files; add BATMAN-HTML.h for Batman battery HTML rendering

---
+++ Software/src/battery/BATMAN-HTML.h
@@ -0,0 +1,19 @@
+#ifndef BATMAN_HTML_H
+#define BATMAN_HTML_H
+
+#include "../devboard/webserver/BatteryHtmlRenderer.h"
+
+class BatmanBattery;  // Forward declaration
+
+class BatmanHtmlRenderer : public BatteryHtmlRenderer {
+ public:
+  explicit BatmanHtmlRenderer(const BatmanBattery& battery) : battery_(battery) {}
+
+  String get_status_html() override;
+  void battery_specific_status(String* content);
+
+ private:
+  const BatmanBattery& battery_;
+};
+
+#endif  // BATMAN_HTML_H 
